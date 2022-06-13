fee54a40553e59205b26d252a7132e09f328919e
Steven Dew <stevendew02@gmail.com>
2022-06-12 21:21:07 -0500

    📦  FEAT: [screens] generate settings and create account screens

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -3,3 +3,5 @@ export * from "./demo/demo-screen"
 export * from "./demo/demo-list-screen"
 export * from "./error/error-boundary"
 // export other screens here
+export * from "./settings/settings-screen"
+export * from "./create-account/create-account-screen"
